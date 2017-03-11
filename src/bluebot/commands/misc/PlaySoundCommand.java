a628f9b2a212bcbcfe5c03089025246ac2dbce6d
thibautbessone <thibaut13b@gmail.com>
2017-03-11 03:11:24 +0100

    Updated PLaySoundCommand.java
    Added options, fixed bugs

--- src/bluebot/commands/misc/PlaySoundCommand.java
+++ src/bluebot/commands/misc/PlaySoundCommand.java
@@ -3,6 +3,7 @@ package bluebot.commands.misc;
 import bluebot.MainBot;
 import bluebot.utils.MyUrlPlayer;
 import bluebot.utils.Command;
+import net.dv8tion.jda.entities.Guild;
 import net.dv8tion.jda.entities.VoiceChannel;
 import net.dv8tion.jda.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.managers.AudioManager;
@@ -10,23 +11,22 @@ import net.dv8tion.jda.managers.AudioManager;
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @file PlaySoundCommand.java
  * @author Blue
- * @version 0.1
- * @brief Makes the bot join the user current voicechannel and play the specified MP3 sound.
+ * @version 0.2
+ * @brief Makes the bot join the user current voice channel and play the specified MP3 sound.
  */
 public class PlaySoundCommand implements Command {
 
-    private MyUrlPlayer urlPlayer = new MyUrlPlayer(MainBot.getJda());
-    private final String HELP = "The command `sound` makes the bot join your current voicechannel and play the specified sound. \n\nUsage : `!sound list` - lists all the available sounds, `!sound theSpecifiedSound` - plays the sound";
+    private Map<Guild, MyUrlPlayer> urlPlayersMap = new HashMap<>();
+    private final String HELP = "The command `sound` makes the bot join your current voice channel and play the specified sound. \n\nUsage : `!sound list` - lists all the available sounds, `!sound theSpecifiedSound` - plays the sound";
     private File folder = new File("soundboard");
-    private File[] listOfFiles;
-    private ArrayList<String> list = new ArrayList<>();
-
-    private AudioManager audioManager;
+    private Map<Guild, AudioManager> audioManagerMap = new HashMap<>();
     private List<VoiceChannel> voiceChannelList;
 
     @Override
@@ -36,49 +36,89 @@ public class PlaySoundCommand implements Command {
 
     @Override
     public void action(String[] args, MessageReceivedEvent event) {
+        if(!urlPlayersMap.containsKey(event.getGuild())) {
+            urlPlayersMap.put(event.getGuild(), new MyUrlPlayer(MainBot.getJda()));
+        }
+
         if(args.length == 0 || args[0].equals("help")) {
             event.getTextChannel().sendMessage(help());
             return;
         } else if(args[0].equals("list")) {
-            listOfFiles = folder.listFiles();
-            for (File file : listOfFiles) {
+            //Displays the sound lists
+            ArrayList<String> list = new ArrayList<>();
+            for (File file : folder.listFiles()) {
                 if(file.getName().contains(".mp3")) {
                     list.add(file.getName().replace(".mp3", ""));
-                    System.out.println(file.getName().replace(".mp3", ""));
-                } else continue;
-
+                }
             }
 
+            String fileList = "```Available sounds :\n" +
+                                 "------------------\n\n";
+            int nb = 0;
+            for(String file : list) {
+                nb++;
+                while(file.length() < 15) {
+                    file += " ";
+                }
+                fileList += file + "\t";
+                if(0 == nb%6) fileList += "\n";
+            }
+            fileList += "```";
+            event.getTextChannel().sendMessage(fileList);
+
+
         } else if(args[0].equals("stop")) {
-            if(urlPlayer.isPlaying()) {
-                urlPlayer.reset();
-                audioManager.closeAudioConnection();
+            if(audioManagerMap.get(event.getGuild()).isConnected()) {
+                urlPlayersMap.get(event.getGuild()).reset();
+                audioManagerMap.get(event.getGuild()).closeAudioConnection();
             } else {
                 event.getTextChannel().sendMessage("I'm not even playing :cry:");
             }
 
+        } else if(args[0].equals("volume")) {
+        if(urlPlayersMap.get(event.getGuild()).isPlaying()) {
+            urlPlayersMap.get(event.getGuild()).reset();
+            audioManagerMap.get(event.getGuild()).closeAudioConnection();
         } else {
-            if(urlPlayer.isPlaying()) {
+            event.getTextChannel().sendMessage("I'm not even playing :cry:");
+        }
+
+    }
+
+
+
+        else {
+            if(urlPlayersMap.get(event.getGuild()).isPlaying()) {
                 event.getTextChannel().sendMessage("I'm already playing a sound.");
                 return;
             }
-            audioManager = MainBot.getJda().getAudioManager(event.getGuild());
-            voiceChannelList = audioManager.getGuild().getVoiceChannels();
+            AudioManager audioManager = MainBot.getJda().getAudioManager(event.getGuild());
+            audioManagerMap.put(event.getGuild(), audioManager);
+            voiceChannelList = audioManagerMap.get(event.getGuild()).getGuild().getVoiceChannels();
+            boolean userConnected = false;
+            urlPlayersMap.get(event.getGuild()).reset();
 
             for(VoiceChannel channel : voiceChannelList) {
                 if(channel.getUsers().contains(event.getAuthor())) {
-                    audioManager.setSendingHandler(urlPlayer);
-                    urlPlayer.setVolume(1);
+                    userConnected = true;
+                    audioManagerMap.get(event.getGuild()).setSendingHandler(urlPlayersMap.get(event.getGuild()));
+                    urlPlayersMap.get(event.getGuild()).setVolume(1);
                     try {
                         URL file = new File(folder.getName() + "/" + args[0] + ".mp3").toURI().toURL();
-                        urlPlayer.setAudioUrl(file);
-                        urlPlayer.play();
-                        audioManager.openAudioConnection(channel);
-                    } catch (Exception e ) {
+                        urlPlayersMap.get(event.getGuild()).setAudioUrl(file);
+                        urlPlayersMap.get(event.getGuild()).play();
+                        if(!audioManagerMap.get(event.getGuild()).isConnected()) {
+                            audioManagerMap.get(event.getGuild()).openAudioConnection(channel);
+                        }
+
+                    } catch (Exception e) {
                         event.getTextChannel().sendMessage("Incorrect file name, please try again.");
                         return;
                     }
                 }
+            }
+            if(!userConnected) {
+                event.getTextChannel().sendMessage("Join a voice channel first.");
             }
         }
     }
